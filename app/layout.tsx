64118cdc5d5f2ce928fe2158d4518b42424f2578
Ipro <usaranish@gmail.com>
2024-03-09 13:47:16 +0545

    meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@
 import { Provider } from "react-redux";
 import { SessionProvider } from "next-auth/react";
 import { store } from "../app/[component]/[redux]/store";
+import Head from 'next/head';
+
 export default function RootLayout({
   children,
 }: {
@@ -11,6 +13,8 @@ export default function RootLayout({
     <html>
       <head>
         <title>Sabbai</title>
+        <meta name="description" content="Shop Smarter at Sabbai: Your Gateway to Quality Products in Nepal. Developed by Ranish Kunwar, Software Engineer" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body>
         <SessionProvider>
